61e648fda8e8844919481557d58cfb0e043ea56d
Arindo-Mudiare <arindoTmudiare@gmail.com>
2019-06-30 00:52:45 +0100

    set up react 2019 starter kit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="jumbotron text-center">
+        Hello React,Webpack 4 & Babel 7!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
