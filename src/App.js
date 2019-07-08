fbb97b29d235250097221f7549d8b1aa1e4c8861
Arindo-Mudiare <arindoTmudiare@gmail.com>
2019-07-08 18:15:40 +0100

    modified message on App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="container">
       <h1 className="jumbotron text-center">
-        Hello React,Webpack 4 & Babel 7!
+        Hello React,Webpack 4 & Babel 7! Ekabo
       </h1>
     </div>
   );
